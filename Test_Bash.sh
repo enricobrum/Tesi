9ef182577d6ab3f42384df3814b378270936d09f
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-12 17:16:05 +0200

    Prova programma bash

--- Test_Bash.sh
+++ Test_Bash.sh
@@ -5,9 +5,9 @@ dim_payload=("8" "16" "32" "64" "128" "256" "512" "1024" "2048" "4096" "8192" "1
 
 # Itera sui valori di param1
 xterm -e python3 Server_TCP.py &
-sleep 1
+sleep 3
 for param1 in "${dim_payload[@]}"
   do
     # Esegui lo script Python con i parametri correnti
-  python3 Client_TCP.py --payload_size "$dim_payload" &
+  python3 Client_TCP.py --payload_size "$param1" &
 done
