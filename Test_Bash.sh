292c6ec54d55509629b6bf89346e7d40d619c757
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-12 16:42:09 +0200

    Prova programma bash

--- Test_Bash.sh
+++ Test_Bash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Definisci i valori per param1 e param2
-param1_values=("8" "16" "32" "64" "128" "256" "512" "1024" "2048" "4096" "8192" "16384" "32768" "65536")
+# Definisci i valori dimensione payload
+dim_payload=("8" "16" "32" "64" "128" "256" "512" "1024" "2048" "4096" "8192" "16384" "32768" "65536")
 
 # Itera sui valori di param1
-for param1 in "${param1_values[@]}"
+for param1 in "${dim_payload[@]}"
   do
     # Esegui lo script Python con i parametri correnti
-    python3 Client_TCP.py --payload_size "$param1" 
+    gnome-terminal -- bash -c "python3 Client_TCP.py --payload_size "$dim_payload"; exec bash" 
 done
