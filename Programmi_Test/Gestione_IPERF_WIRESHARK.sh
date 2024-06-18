60772a6ccb93c32963a66a55a3ff034222b98ba9
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-18 15:59:59 +0200

    Inizio sviluppo programmi di test: verifica funzione lettura file di configurazione

--- Programmi_Test/Gestione_IPERF_WIRESHARK.sh
+++ Programmi_Test/Gestione_IPERF_WIRESHARK.sh
@@ -5,7 +5,9 @@ get_config_value() {
     local key="$2"
     local config_file="$3"
     local value
-    valure=$(awk '/^\[/ { app=substr($0,2,length-2) } /=/ { print app "." $0 }' file.conf)
+    sed -n \
+    -e '/^\[/{s/[][]//g;h;}' \
+    -e '/=/{H;g;s/\n/./;p;s/\..*//;h;}' file.conf
     echo "$value"
 }
 
@@ -13,10 +15,10 @@ get_config_value() {
 #Leggo i valori dei file di configurazione:
 CONFIG_FILE="config.ini"
 # Parametri nodo centrale
-echo CENTRAL_IP=$(get_config_value "nodo_centrale" "ip" "$CONFIG_FILE")
-echo DURATION=$(get_config_value "nodo_centrale" "duration" "$CONFIG_FILE")
-echo INTERFACE=$(get_config_value "nodo_centrale" "interface" "$CONFIG_FILE")
-echo TSHARK_OUTPUT_FILE=$(get_config_value "nodo_centrale" "tshark_output_file" "$CONFIG_FILE")
+echo CENTRAL_IP=$(get_config_value "nodo_centrale" "ip" CONFIG_FILE)
+echo DURATION=$(get_config_value "nodo_centrale" "duration" CONFIG_FILE)
+echo INTERFACE=$(get_config_value "nodo_centrale" "interface" CONFIG_FILE)
+echo TSHARK_OUTPUT_FILE=$(get_config_value "nodo_centrale" "tshark_output_file" CONFIG_FILE)
 # Parametri server
 echo SERVER1_IP=$(get_config_value "server1" "ip" "$CONFIG_FILE")
 echo SERVER1_PORT=$(get_config_value "server1" "port" "$CONFIG_FILE")
