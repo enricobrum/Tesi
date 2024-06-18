5ee864a373fd34f555c5a6b100512a546c59fc77
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-18 15:27:22 +0200

    Inizio sviluppo programmi di test: verifica funzione lettura file di configurazione

---
+++ Programmi_Test/Gestione_IPERF_WIRESHARK.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Funzione per leggere da file di configurazione
+get_config_value(){
+    section=$1
+    key=$2
+    config_file=$3
+    value=$(sed -n "/^\[$section\]/,/^\[/{/^[^#].*=/p;}" "$config_file" | grep "^$key=" | cut -d '=' -f2 | tr -d ' ')
+    echo $value
+}
+#Leggo i valori dei file di configurazione:
+# Parametri nodo centrale
+echo CENTRAL_IP=$(get_config_value "nodo_centrale" "ip" "$CONFIG_FILE")
+echo DURATION=$(get_config_value "nodo_centrale" "duration" "$CONFIG_FILE")
+echo INTERFACE=$(get_config_value "nodo_centrale" "interface" "$CONFIG_FILE")
+echo TSHARK_OUTPUT_FILE=$(get_config_value "nodo_centrale" "tshark_output_file" "$CONFIG_FILE")
+# Parametri server
+echo SERVER1_IP=$(get_config_value "server1" "ip" "$CONFIG_FILE")
+echo SERVER1_PORT=$(get_config_value "server1" "port" "$CONFIG_FILE")
+echo SERVER1_IPERF_OUTPUT_FILE=$(get_config_value "server1" "iperf_output_file" "$CONFIG_FILE")
+echo SERVER2_IP=$(get_config_value "server2" "ip" "$CONFIG_FILE")
+echo SERVER2_PORT=$(get_config_value "server2" "port" "$CONFIG_FILE")
+echo SERVER2_IPERF_OUTPUT_FILE=$(get_config_value "server2" "iperf_output_file" "$CONFIG_FILE")
+
