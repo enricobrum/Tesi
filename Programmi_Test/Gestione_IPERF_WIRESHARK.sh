5bccbfc0e96a54b96f57fca2a556f97ba9d4ec4a
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-18 16:15:31 +0200

    Inizio sviluppo programmi di test: verifica funzione lettura file di configurazione

--- Programmi_Test/Gestione_IPERF_WIRESHARK.sh
+++ Programmi_Test/Gestione_IPERF_WIRESHARK.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 #Funzione per leggere da file di configurazione
-get_config_value() {
-    local section="$1"
-    local key="$2"
-    local config_file="$3"
-    local value
-    sed -n \
-    -e '/^\[/{s/[][]//g;h;}' \
-    -e '/=/{H;g;s/\n/./;p;s/\..*//;h;}' file.conf
-    echo "$value"
+function ini_printdb {
+    for i in "${!inidb[@]}"
+    do
+     split the associative key in to section and key
+       echo -n "section  : $(echo $i | cut -f1 -d ' ');"
+       echo -n "key  : $(echo $i | cut -f2 -d ' ');"
+       echo  "value: ${inidb[$i]}"
+    done
 }
+function ini_get_value {
+    section=$1
+    key=$2
+    echo "${inidb[$section $key]}"
+}
+ini_loadfile test.ini
+ini_printdb
 
 
-#Leggo i valori dei file di configurazione:
-CONFIG_FILE="config.ini"
-# Parametri nodo centrale
-echo CENTRAL_IP=$(get_config_value "nodo_centrale" "ip" CONFIG_FILE)
-echo DURATION=$(get_config_value "nodo_centrale" "duration" CONFIG_FILE)
-echo INTERFACE=$(get_config_value "nodo_centrale" "interface" CONFIG_FILE)
-echo TSHARK_OUTPUT_FILE=$(get_config_value "nodo_centrale" "tshark_output_file" CONFIG_FILE)
-# Parametri server
-echo SERVER1_IP=$(get_config_value "server1" "ip" "$CONFIG_FILE")
-echo SERVER1_PORT=$(get_config_value "server1" "port" "$CONFIG_FILE")
-echo SERVER1_IPERF_OUTPUT_FILE=$(get_config_value "server1" "iperf_output_file" "$CONFIG_FILE")
-echo SERVER2_IP=$(get_config_value "server2" "ip" "$CONFIG_FILE")
-echo SERVER2_PORT=$(get_config_value "server2" "port" "$CONFIG_FILE")
-echo SERVER2_IPERF_OUTPUT_FILE=$(get_config_value "server2" "iperf_output_file" "$CONFIG_FILE")
 
