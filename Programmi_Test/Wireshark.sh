d8b0740b1d4666ef8f5d843ccc5b3f6a13ef9fba
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-19 17:56:08 +0200

    script per l'avvio di wireshark in ascolto e configurazione iperf

--- Programmi_Test/Wireshark.sh
+++ Programmi_Test/Wireshark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function avvio_tshark(){
-    interfaccia = $1
-    durata = $2
-    output_file = $3
+    interfaccia=$1
+    durata=$2
+    output_file=$3
     echo "Avvio cattura dei pacchetti sull'interfaccia $WIRESHARK_INTERFACCIA per $WIRESHARK_DURATA s. I dati vengono salvati in $WIRESHARK_OUTPUT_FILE ..."
     tshark -i $interfaccia -a duration:$durata -w $output_file &
     echo $!
