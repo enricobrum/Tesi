2fdb19dd0112f9c30443a99ebd7ceec9cd7f68b1
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-19 15:15:54 +0200

    Sviluppo programma per avviare client e server tramite iperf

--- Programmi_Test/Iperf.sh
+++ Programmi_Test/Iperf.sh
@@ -5,12 +5,12 @@ IPER_SERVER_IP=$(ini_get_value iperf ip)
 IPER_SERVER_PORT=$(ini_get_value iperf port)
 IPER_SERVER_OUTPUT_FILE=$(ini_get_value iperf output_file)
 start_iperf_server(){
-    local ip=$1
-    local port=$2
-    local output_file=$3
+    ip=$1
+    port=$2
+    output_file=$3
     echo "Avvio del server iperf su $ip:$port..."
     iperf3 -s -B $ip -p $port > $output_file 2>&1 &
     echo $! # Restituisce il PID del processo avviato
 }
-IPERF_SERVER_PID=$(start_iperf_server IPERF_SERVER_IP IPERF_SERVER_PORT IPERF_SERVER_OUTPUT_FILE)
+IPERF_SERVER_PID=$(start_iperf_server $IPERF_SERVER_IP $IPERF_SERVER_PORT $IPERF_SERVER_OUTPUT_FILE)
 kill $IPERF_SERVER_PID
