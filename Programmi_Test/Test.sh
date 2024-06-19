4c9ded74c8faa6ea23e84e69fc55501928b64e75
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-19 10:29:13 +0200

    Inizio sviluppo programmi di test: verifica funzione lettura file di configurazione

--- Programmi_Test/Test.sh
+++ Programmi_Test/Test.sh
@@ -3,3 +3,7 @@
 source ./Lettura_File_Config.sh
 valore=$(ini_get_value server1 port)
 echo "value: $valore"
+valore=$(ini_get_value server2 port)
+echo "value: $valore"
+valore=$(ini_get_value server1 port)
+echo "value: $valore"
