5c8d70718148aa74edf2ece3359bb4bad5c2eaae
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-20 10:00:53 +0200

    script per l'avvio di wireshark in ascolto e configurazione iperf

--- Programmi_Test/Test_Network.sh
+++ Programmi_Test/Test_Network.sh
@@ -4,11 +4,12 @@ source ./Lettura_File_Config.sh
 . Wireshark.sh
 #___________________________________________________________
 #Costanti ottenute tramite il file di configuraazione
-IPERF_SERVER_IP=$(ini_get_value iperf ip)
+IPERF_SERVER_IP=$(ini_get_value iperf ip_server)
 IPERF_SERVER_PORT=$(ini_get_value iperf server_port)
 IPERF_SERVER_OUTPUT_FILE=$(ini_get_value iperf output_file)
 IPERF_CLIENT_DURATION=$(ini_get_value iperf client_duration)
 IPERF_CLIENT_PORT=$(ini_get_value iperf client_port)
+IPERF_CLIENT_IP=$(ini_get_value iperf ip_client)
 WIRESHARK_INTERFACCIA=$(ini_get_value wireshark interfaccia)
 WIRESHARK_DURATA=$(ini_get_value wireshark durata)
 WIRESHARK_OUTPUT_FILE=$(ini_get_value wireshark output_file)
@@ -18,7 +19,7 @@ SERVER_PID=$!
 sleep 5
 avvio_tshark $WIRESHARK_INTERFACCIA $WIRESHARK_DURATA $WIRESHARK_OUTPUT_FILE
 TSHARK_PID=$!
-start_iperf_client $IPERF_SERVER_IP $IPERF_CLIENT_PORT $IPERF_CLIENT_DURATION
+start_iperf_client $IPERF_CLIENT_IP $IPERF_CLIENT_PORT $IPERF_CLIENT_DURATION
 wait $TSHARK_PID
 if [ $? -eq 0 ]; then
     echo "Cattura completata. File salvato in $OUTPUT_FILE"
