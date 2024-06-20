79bdc80e216705ad7480cb838f981d290860be48
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-20 16:10:35 +0200

    script per l'avvio di wireshark in ascolto e configurazione iperf

--- Programmi_Test/Iperf.sh
+++ Programmi_Test/Iperf.sh
@@ -5,7 +5,7 @@ start_iperf_server(){
     local port=$2
     echo "Avvio del server iperf su $ip : $port ..."
     iperf3 -s -B $ip -p $port 
-    echo $! # Restituisce il PID del processo avviato
+   # echo $! # Restituisce il PID del processo avviato
 }
 #Funzione per l'avvio di un client IPERF passando IP del server, PORT e durata in secondi della comunicazione
 start_iperf_client() {
@@ -14,7 +14,7 @@ start_iperf_client() {
     local duration=$3
     echo "Avvio del client iperf verso $ip:$port per $duration secondi..."
     iperf3 -c $ip -p $port -t $duration 
-    echo $!
+ #   echo $!
 }
 
 
