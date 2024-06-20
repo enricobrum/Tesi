6afdbc87ff63feb56071f2f09285f9ca7dd42a86
enricobrum <e.brunelli010@studenti.unibs.it>
2024-06-20 09:44:32 +0200

    script per l'avvio di wireshark in ascolto e configurazione iperf

--- Programmi_Test/Test_Network.sh
+++ Programmi_Test/Test_Network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source Lettura_File_Config.sh
-source Iperf.sh
-source Wireshark.sh
+. Lettura_File_Config.sh
+. Iperf.sh
+. Wireshark.sh
 #___________________________________________________________
 #Costanti ottenute tramite il file di configuraazione
 IPERF_SERVER_IP=$(ini_get_value iperf ip)
