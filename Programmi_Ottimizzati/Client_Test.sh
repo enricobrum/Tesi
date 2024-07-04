3b736d6d4d422dbae9b7ab69da13ea569586f4d8
enricobrum <e.brunelli010@studenti.unibs.it>
2024-07-04 10:12:24 +0200

    Aggiornamento programmi di test

--- Programmi_Ottimizzati/Client_Test.sh
+++ Programmi_Ottimizzati/Client_Test.sh
@@ -16,9 +16,9 @@ IPERF_SERVER_PORT=$(ini_get_value iperf server_port)
 for dim in "${dim_bw[@]}" #loop che varia il traffico generato dal client iperf
   do
     #chiamata a funzione di avvio del client con i parametri passati
-    start_iperf_client $IPERF_SERVER_IP $IPERF_CLIENT_PORT $IPERF_CLIENT_DURATION $dim & #salvataggio del pid del processo client iperf in modo da poter sapere quando è terminato
-    pid=$!
-    echo "Client iperf avvianto al: $IPERF_SERVER_IP:$IPERF_SERVER_PORT per $IPERF_CLIENT_DURATION s con $dim di bitrate."
+    #start_iperf_client $IPERF_SERVER_IP $IPERF_CLIENT_PORT $IPERF_CLIENT_DURATION $dim & #salvataggio del pid del processo client iperf in modo da poter sapere quando è terminato
+    #pid=$!
+    #echo "Client iperf avvianto al: $IPERF_SERVER_IP:$IPERF_SERVER_PORT per $IPERF_CLIENT_DURATION s con $dim di bitrate."
     for param1 in "${dim_payload[@]}" #loop che varia la dimensione dei payload dei messaggi scambiati tra client e server
       do
         python3 Client.py --server_host "$ip_server" --server_port "$port_server" --payload_size "$param1" --type_test "$dim" #programma python che avvia il client di test
