4d181e692cbcfb6067388d76a4f9ecf5ad16e46d
enricobrum <e.brunelli010@studenti.unibs.it>
2024-07-04 10:34:52 +0200

    Aggiornamento programmi di test

--- Programmi_Ottimizzati/Client_Test.sh
+++ Programmi_Ottimizzati/Client_Test.sh
@@ -6,18 +6,18 @@ chmod +x Lettura_File_Config.sh
 source Lettura_File_Config.sh
 # Definisci i valori dimensione payload
 dim_payload=("8" "16" "32" "64" "128" "256" "512" "1024" "1500")
-dim_bw=("10M" "100M" "1G" "9G")
+dim_bw=("80M" "800M" "8G" "72G")
 ip_server=$(ini_get_value server ip)
 port_server=$(ini_get_value server port)
 IPERF_CLIENT_DURATION=$(ini_get_value iperf client_duration)
 IPERF_CLIENT_PORT=$(ini_get_value iperf client_port)
 IPERF_SERVER_IP=$(ini_get_value iperf ip_server)
 IPERF_SERVER_PORT=$(ini_get_value iperf server_port)
-for dim in "${dim_bw[@]}" #loop che varia il traffico generato dal client iperf
+for dim in "${dim_bw[@]}" #loop che varia il traffico generato dal client iperf 
   do
     #chiamata a funzione di avvio del client con i parametri passati
     start_iperf_client $IPERF_SERVER_IP $IPERF_CLIENT_PORT $IPERF_CLIENT_DURATION $dim & #salvataggio del pid del processo client iperf in modo da poter sapere quando è terminato
-    #pid=$!
+    pid=$!
     #echo "Client iperf avvianto al: $IPERF_SERVER_IP:$IPERF_SERVER_PORT per $IPERF_CLIENT_DURATION s con $dim di bitrate."
     for param1 in "${dim_payload[@]}" #loop che varia la dimensione dei payload dei messaggi scambiati tra client e server
       do
