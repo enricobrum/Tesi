0428ec16fdf70a7fafd14bb8679798f363e532dc
enricobrum <e.brunelli010@studenti.unibs.it>
2024-07-15 10:03:07 +0200

    Sviluppo client tcp in modo da poter selezionare le varie tipologie di test

--- Programmi_Set_Up_Test/Client.py
+++ Programmi_Set_Up_Test/Client.py
@@ -5,7 +5,7 @@
 #il funzionamento dell'infrastruttura 5G privata.
 #_________________________________________________________
 from datetime import datetime
-
+from ping3 import ping
 import time
 import socket #libreria necessaria all'utilizzo dei 
               #protocolli di comunicazione
@@ -15,51 +15,87 @@ import Utility #libreria personale contenente la funzione
                #"time_stamp()" che permette di estrarre i
                #secondi e microsendi dall'orologio locale
 #________________________________________________________
+#Definizione delle costanti utilizzate per i test
+#Dimensione del payload (byte)
+PAYLOAD=[8,16,32,64,128,256,512,1024,1500]
 #Funzione per avviare il client tcp tentando la connessione
 #con l'indirizzo IP del server passato come argomento, la 
 #porta in cui e' in ascolto e la dimensione, in byte, del 
 #messaggio da inviare. Infine e' anche passata la tipologia
 #di test che si sta effettuando in modo da tenere conto 
 #dello scenario da cui provengono i futuri dati raccolti
-timetowait=[0.01,0.1,1,5,10]
-def send_receive(payload,payload_size,type_test,client_socket,file):
+#Vettore contenente gli intertempi di test
+INTERTEMPO=[0.01,0.1,1,5,10]
+#Funzione per connettersi al server
+def connect_to_server(host, port):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        return client_socket
+    except Exception as e:
+        print(f"Errore nella connessione al server: {e}")
+        return None
+#Funzione per selezionare la tipologia di test
+def select_test():
+    print("Seleziona il test da eseguire:")
+    print("1. Test variazione payload e intertempo")
+    print("2. Test variazione payload e intertempo + traffico Iperf")
+    print("3. Test di throughput (sfruttando libreria 'ping3' python)")
+    print("4. Test di latenza (sfruttando la libreria 'time' di python)")
+    choice = input("Inserisci il numero del test: ")
+    return choice
+#Funzione di test di variazione payload e intertempo
+def test_payload_inter(client_socket,file,type_test):
+    #Impostiamo il messaggio da inviare pari ai byte del payload
+    for intertempo in INTERTEMPO:
+        for payload_size in PAYLOAD:
+            payload=b'a'*payload_size
             sec1,us1=Utility.time_stamp()
+            tempo_avvio = time.time()
             client_socket.sendall(payload)
-        #   print(f"Inviati {payload_size} bytes")
-            data=client_socket.recv(65536) #buffer impostato al massimo
-            #in modo da effettuare test con variazione del payload
+            data=client_socket.recv(65536)
             sec2,us2=Utility.time_stamp()
             file.write(str(sec1)+'.'+str(us1)+',')
             file.write(str(sec2)+'.'+str(us2)+',')
             file.write(str(payload_size)+','+type_test+',')
-            rtt_sec=sec2-sec1
-            if rtt_sec >= 0:
+            if sec2 - sec1 == 0:
                 rtt_us=us2-us1
             else:
-                rtt_us=(us2-us1)
+                rtt_us=us1-us2+1000000
             file.write(str(rtt_us))
             file.write("\n")
-            
+            tempo_rimanente = time.time() - tempo_avvio
+            time.sleep(intertempo-tempo_rimanente)
+          
+#Funzione principale del client
 def tcp_client(server_host,server_port,payload_size,type_test):
-    data_corrente = datetime.now()
-    data_stringa = data_corrente.strftime("%Y-%m-%d")
-    filecsv="istanti_temporali_"+data_stringa+".csv"
-    file=open(filecsv,"a")
-    if file.tell()==0:
-        file.write("Inviato,Ricevuto,PackSize,Traffico,RTT\n")
-        print("File csv creato.")
-    try:
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((server_host,server_port))
-        #   print(f"Connessione al server TCP {server_host}:{server_port}")
-            payload=b'a'*payload_size    
-            for waiting in timetowait:
-                send_receive(payload,payload_size,type_test,client_socket,file)
-                time.sleep(waiting)
-                #   print(f"Ricevuto {len(data)} bytes da {server_host}:{server_port}")
-            file.close()
-    except Exception as e:
-        print(f"Eccezione {e} durante la connessione con il server.")
+    client_socket = connect_to_server(server_host,server_port)
+    if not client_socket:
+        print("Impossibile connettersi al server. Uscita.")
+        return
+    else:
+        data_corrente = datetime.now()
+        data_stringa = data_corrente.strftime("%Y-%m-%d")
+        filecsv="istanti_temporali_"+data_stringa+".csv"
+        file=open(filecsv,"a")
+        if file.tell()==0:
+            file.write("Inviato,Ricevuto,PackSize,Traffico,RTT\n")
+            print("File csv creato.")
+        while True:
+            ntest = select_test()
+            if ntest == '1':
+                test_payload_inter(client_socket,file,type_test)
+            elif ntest == '2':
+                continue
+            elif ntest == '3':
+                continue                
+            elif ntest == '4':
+                continue
+            else:
+                print("Scelta non valida.")
+                continue
+    
+
 
 
 #_________________________________________________________________
