9de60d3a3debcfa88bfd86633ce7c368bc22f494
enricobrum <e.brunelli010@studenti.unibs.it>
2024-08-08 19:53:29 +0200

    Sviluppo programmi di test finali client e server

--- Set_Up_Finale/Start_Client.sh
+++ Set_Up_Finale/Start_Client.sh
@@ -6,6 +6,7 @@ ip_server=$(ini_get_value server ip)
 port_tcp=$(ini_get_value server port_tcp)
 port_udp=$(ini_get_value server port_udp)
 intervals=$(ini_get_value client intervals)
+traffic=$(ini_get_value client traffic)
 #____________________________________________
-python3 Client_finale.py --server_host "$ip_server" --tcp_port "$port_tcp" --udp_port "$port_udp" --intervals "$intervals"
+python3 Client_finale.py --server_host "$ip_server" --tcp_port "$port_tcp" --udp_port "$port_udp" --intervals "$intervals" --traffic "$traffic"
 wait $!
